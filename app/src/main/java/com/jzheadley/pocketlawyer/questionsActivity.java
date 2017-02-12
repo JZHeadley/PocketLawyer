b0289cd59e447322770a0aa230b3184608bf270a
parrycj <parrycj@vcu.edu>
2017-02-11 23:12:21 -0500

    Tweeking with scenarios

--- app/src/main/java/com/jzheadley/pocketlawyer/questionsActivity.java
+++ app/src/main/java/com/jzheadley/pocketlawyer/questionsActivity.java
@@ -1,21 +1,21 @@
 package com.jzheadley.pocketlawyer;
 
-        import android.app.Activity;
-        import android.app.Dialog;
-        import android.content.Context;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.view.View.OnClickListener;
-        import android.widget.Button;
-        import android.widget.ImageView;
-        import android.widget.TextView;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class questionsActivity extends Activity{
+public class questionsActivity extends Activity {
 
     final Context context = this;
     private Button button;
 
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questions);
 
@@ -26,28 +26,41 @@ public class questionsActivity extends Activity{
             @Override
             public void onClick(View v) {
                 //custom dialog
-                final Dialog dialog = new Dialog(context);
-                dialog.setContentView(R.layout.scenario1);
-                dialog.setTitle("Scenario1");
 
-                //custom dialog settings
-                TextView text = (TextView) dialog.findViewById(R.id.text);
-                text.setText("Scenario 1 works!");
-                //ImageView image = (ImageView) dialog.findViewById(R.id.image);
-                //image.setImageResource(R.drawable.);
-
-                Button dialogButton = (Button) dialog.findViewById(R.id.done_popup_button);
-
-                dialogButton.setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
-            dialog.show();
             }
         });
     }
 
+    private void displayDialog(String title, String text) {
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.scenario1);
+        dialog.setTitle(title);
 
+        TextView textView = (TextView) dialog.findViewById(R.id.text);
+        textView.setText(text);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.done_popup_button);
+
+        dialogButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.scenario1:
+                displayDialog("Scenario1",getString(R.string.scenario1_text));
+                break;
+            case R.id.scenario2:
+                displayDialog("Scenario2",getString(R.string.scenario2_text));
+                break;
+            case R.id.scenario3:
+                displayDialog("Scenario3",getString(R.string.scenario3_text));
+                break;
+        }
+    }
 }
