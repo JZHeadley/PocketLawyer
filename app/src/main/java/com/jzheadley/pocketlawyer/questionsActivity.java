c5cb73823ac01441ff62faefe28bab0e6113d6d0
parrycj <parrycj@vcu.edu>
2017-02-12 00:52:11 -0500

    Popups complete without titles

--- app/src/main/java/com/jzheadley/pocketlawyer/questionsActivity.java
+++ app/src/main/java/com/jzheadley/pocketlawyer/questionsActivity.java
@@ -7,35 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-public class questionsActivity extends Activity {
+public class questionsActivity extends Activity implements View.OnClickListener {
 
     final Context context = this;
-    private Button button;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questions);
 
-        button = (Button) findViewById(R.id.scenario1);
-
-        //add button listener
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //custom dialog
-
-            }
-        });
     }
 
-    private void displayDialog(String title, String text) {
+    private void displayDialog(String title, String text, int layoutID) {
         final Dialog dialog = new Dialog(context);
-        dialog.setContentView(R.layout.scenario1);
+        dialog.setContentView(layoutID);
         dialog.setTitle(title);
-
+// TODO: Find out why title doesn't appear 
         TextView textView = (TextView) dialog.findViewById(R.id.text);
         textView.setText(text);
 
@@ -50,16 +38,17 @@ public class questionsActivity extends Activity {
         dialog.show();
     }
 
+    @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.scenario1:
-                displayDialog("Scenario1",getString(R.string.scenario1_text));
+                displayDialog("Scenario1",getString(R.string.scenario1_text), R.layout.scenario1);
                 break;
             case R.id.scenario2:
-                displayDialog("Scenario2",getString(R.string.scenario2_text));
+                displayDialog("Scenario2",getString(R.string.scenario2_text), R.layout.scenario2);
                 break;
             case R.id.scenario3:
-                displayDialog("Scenario3",getString(R.string.scenario3_text));
+                displayDialog("Scenario3",getString(R.string.scenario3_text), R.layout.scenario3);
                 break;
         }
     }
