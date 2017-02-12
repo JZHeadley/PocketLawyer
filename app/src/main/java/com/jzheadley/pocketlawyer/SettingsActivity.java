fb96678650ddd61c9e384674040bba04d158cbee
jzheadley <zephyrinkg@gmail.com>
2017-02-12 09:32:52 -0500

    Now... Finally... That stupid Q will be fixed

--- app/src/main/java/com/jzheadley/pocketlawyer/SettingsActivity.java
+++ app/src/main/java/com/jzheadley/pocketlawyer/SettingsActivity.java
@@ -3,11 +3,14 @@ package com.jzheadley.pocketlawyer;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
 public class SettingsActivity extends BaseActivity {
+    private static final String TAG = "SettingsActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,17 +26,17 @@ public class SettingsActivity extends BaseActivity {
         }
     }
 
-
     public void onClick(View v) {
         SharedPreferences preferences = getSharedPreferences("PocketLawyer", MODE_PRIVATE);
         SharedPreferences.Editor edit = preferences.edit();
         String ethnicity = ((EditText) findViewById(R.id.ethnicity_et)).getText().toString();
         String username = ((EditText) findViewById(R.id.username_et)).getText().toString();
         boolean gender = (((RadioGroup) findViewById(R.id.gender_radio_group)).getCheckedRadioButtonId() == R.id.female);
+        Log.d(TAG, "onClick: Gender:" + gender);
         edit.putBoolean("isFemale", gender);
         edit.putString("ethnicity", ethnicity);
         edit.putString("username", username);
-        edit.commit();
+        edit.apply();
         finish();
     }
 }
