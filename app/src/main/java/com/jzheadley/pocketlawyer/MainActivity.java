405ada9a4431ebab698ee5d6c4e2c6c4aade755a
jzheadley <zephyrinkg@gmail.com>
2017-02-11 20:39:19 -0500

    Can now turn on and off voice recognition with methods...Threads are painful

--- app/src/main/java/com/jzheadley/pocketlawyer/MainActivity.java
+++ app/src/main/java/com/jzheadley/pocketlawyer/MainActivity.java
@@ -5,25 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.jzheadley.pocketlawyer.data.services.SpeechToTextService;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private SpeechToText speechService;
-    private MicrophoneInputStream capture;
     private Button recordButton;
+    private SpeechToTextService speechToTextService;
+    private Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recordButton = (Button) findViewById(R.id.btn_record);
+        nextButton = (Button) findViewById(R.id.btn_next);
+        speechToTextService = new SpeechToTextService();
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                (new SpeechToTextService()).startRecording();
+                speechToTextService.startRecording();
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    speechToTextService.stopRecording();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
--- app/src/main/java/com/jzheadley/pocketlawyer/data/services/SpeechToTextService.java
+++ app/src/main/java/com/jzheadley/pocketlawyer/data/services/SpeechToTextService.java
@@ -1,24 +1,35 @@
 package com.jzheadley.pocketlawyer.data.services;
 
+import android.util.Log;
+
 import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
 import com.ibm.watson.developer_cloud.android.library.audio.utils.ContentType;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.jzheadley.pocketlawyer.callbacks.SpeechReceivedCallback;
 import com.jzheadley.pocketlawyer.data.singletons.SpeechToTextClient;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpeechToTextService {
 
+    private static final String TAG = "SpeechToTextService";
     private List<String> keywords;
+    private SpeechCaptureThread thread;
+
+    public SpeechToTextService() {
+        keywords = new ArrayList<>();
+    }
 
     public void startRecording() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                SpeechToTextClient.getInstance().recognizeUsingWebSocket(new MicrophoneInputStream(true), getRecognizeOptions(), new SpeechReceivedCallback());
-            }
-        }).start();
+        thread = new SpeechCaptureThread();
+        thread.run();
+    }
+
+    public void stopRecording() throws InterruptedException, IOException {
+        Log.d(TAG, "stopRecording: Stop listening Watson");
+        thread.end();
     }
 
     public void setKeywords(List<String> keywords) {
@@ -26,13 +37,34 @@ public class SpeechToTextService {
     }
 
     private RecognizeOptions getRecognizeOptions() {
-        return new RecognizeOptions.Builder()
-                .continuous(true)
-                .keywords((String[]) keywords.toArray())
-                .contentType(ContentType.OPUS.toString())
-                .model("en-US_BroadbandModel")
-                .interimResults(true)
-                .inactivityTimeout(2000)
-                .build();
+        RecognizeOptions.Builder options = new RecognizeOptions.Builder()
+            .continuous(true)
+            .contentType(ContentType.OPUS.toString())
+            .model("en-US_BroadbandModel")
+            .interimResults(true)
+            .profanityFilter(false)
+            .inactivityTimeout(2000);
+        if (keywords.size() > 0) {
+            options.keywords((String[]) keywords.toArray());
+        }
+        return options.build();
+    }
+
+    private class SpeechCaptureThread extends Thread {
+        private MicrophoneInputStream capture;
+
+        public SpeechCaptureThread() {
+            capture = new MicrophoneInputStream(true);
+        }
+
+        @Override
+        public void run() {
+            SpeechToTextClient.getInstance().recognizeUsingWebSocket(capture, getRecognizeOptions(), new SpeechReceivedCallback());
+        }
+
+        public void end() throws IOException {
+            capture.close();
+            Log.d(TAG, "end: Trying to stop the recorder");
+        }
     }
 }
